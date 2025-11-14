b509996e334e187aed55c5cb3adb17d7a5bbe7c1
julio serafim <juliodev26@gmail.com>
2025-11-14 14:09:38 +0100

    criando peticion a api

--- js/script.js
+++ js/script.js
@@ -12,14 +12,33 @@ let temperature;
 let windSpeed;
 let precipitation;
 
-function setCitys(cityEl) {
 
+
+
+
+
+
+
+
+
+function setCitys(cityId,citys) {
+   citys.forEach(el => {
+      if(cityId == el.name){
+         fetch(`https://api.open-meteo.com/v1/forecast?
+              latitude=${el.lat}&longitude=${el.long}&current=precipitation,
+               temperature_2m,relative_humidity_2m,wind_speed_10m&
+               wind_speed_unit=mph&temperature_unit=fahrenheit&
+               precipitation_unit=inch`).then(res => res.json()).then(json => {
+               console.log(json)
+          })
+        }
+   })
 }
 const city = [
    {
       name: 'Sevilla',
-      lat: 37.3828,
-      long: -5.9732
+      lat:37.3828,
+      long:-5.9732
    },
    {
       name: 'Cadiz',
@@ -45,45 +64,70 @@ window.addEventListener("click", (e) => {
       navImMe.classList.toggle("onDisplay");
    }
 
-   if (e.target.matches("#search-input")) {
-      search.classList.add("onDisplay");
-      while (search.querySelectorAll("button").length == 0) {
-         city.forEach(e => {
-            templeteCitys.querySelector("button").textContent = e.name;
-            let clone = d.importNode(templeteCitys, true);
-            fragment.appendChild(clone);
-         })
-         search.appendChild(fragment);
-      }
-   }
-   if (!e.target.matches("#search-input")) {
+   // if (e.target.matches("#search-input")) {
+      // search.classList.add("onDisplay");
+      // while (search.querySelectorAll("button").length == 0) {
+         // city.forEach(e => {
+            // templeteCitys.querySelector("button").textContent = e.name;
+            // templeteCitys.querySelector('button').id = e.name;
+            // let clone = d.importNode(templeteCitys, true);
+            // fragment.appendChild(clone);
+         // })
+         // search.appendChild(fragment);
+      // }
+   // }
+   if (!e.target.matches("#search-but")) {
       search.classList.remove("onDisplay");
       search.replaceChildren();
    }
 
-
-})
-searchInput.addEventListener("keydown", (e) => {
-   if (e.target.value != "") {
-      let valueSearch = e.target.value;
-      search.replaceChildren();
-      city.forEach(e => {
-         if (e.name.toLowerCase().includes(valueSearch.toLowerCase())) {
-            templeteCitys.querySelector("button").textContent = e.name;
-            let clone = d.importNode(templeteCitys, true);
-            fragment.appendChild(clone);
+   if (e.target.matches("#search-but")) {
+      console.log(searchInput.value)
+      let valueSearch = searchInput.value;
+   // search.replaceChildren();
+       if(searchInput.value != ""){
+         city.forEach(e => {
+            if (e.name.toLowerCase().includes(valueSearch.toLowerCase())) {
+                templeteCitys.querySelector("button").textContent = e.name;
+                templeteCitys.querySelector("button").id = e.name;
+                let clone = d.importNode(templeteCitys, true);
+                fragment.appendChild(clone);
+               }
+             })
+            search.appendChild(fragment);
+            search.classList.add("onDisplay");
          }
-      })
-      search.appendChild(fragment);
    }
-    
-   
 
+   // setCitys(e.target, city);
+   let info = setCitys(e.target.id, city);
+   console.log(info)
 })
-//fetch("https://api.open-meteo.com/v1/forecast?latitude=40&longitude=-4&hourly=temperature_2m,wind_speed_10m,temperature_80m,wind_direction_10m,relative_humidity_2m,apparent_temperature&timezone=Europe%2FBerlin&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch&bounding_box=-90,-180,90,180")
-// .then(res => res.json()).then(res => {
-//     temperature
+
+
+// sea.addEventListener("keydown", (e) => {
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+      
 // })
+
+
+
+
+  
+  
+//
 
 
 
